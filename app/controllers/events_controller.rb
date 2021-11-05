b59cc3ef5223c0fba49fc65351317f7e3e806b7e
sayakanoa <reiluv75418@gmail.com>
2021-11-05 08:58:15 +0000

    カレンダー機能new/index/create追加

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,4 +1,25 @@
 class EventsController < ApplicationController
-  def index
+
+  def new
+    @event = Event.new
   end
+
+  def index
+    @events = Event.all
+  end
+
+  def create
+    @event = Event.new(event_parameter)
+    @event.user_id = current_user.id
+    @event.save
+    redirect_to events_path
+  end
+
+  private
+
+  def event_parameter
+    params.require(:event).permit(:title, :body, :start_time)
+  end
+
+
 end
