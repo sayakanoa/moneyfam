cc2aa9844e224acce0efe8840f91743d4a4ee19a
sayakanoa <reiluv75418@gmail.com>
2021-11-08 02:57:02 +0000

    カレンダー削除機能追加

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -15,6 +15,12 @@ class EventsController < ApplicationController
     redirect_to events_path
   end
 
+  def destroy
+    event = Event.find(params[:id])
+    event.destroy
+    redirect_to events_path
+  end
+
   private
 
   def event_parameter
