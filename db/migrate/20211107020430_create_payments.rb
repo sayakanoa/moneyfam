6026f1ee7eb00fed88ba33a9ffcc7d51134d26bd
sayakanoa <reiluv75418@gmail.com>
2021-11-20 03:09:55 +0000

    payment/migrate編集

--- db/migrate/20211107020430_create_payments.rb
+++ db/migrate/20211107020430_create_payments.rb
@@ -3,13 +3,13 @@ class CreatePayments < ActiveRecord::Migration[5.2]
     create_table :payments do |t|
       t.references :user, foreign_key: true
       t.date :month, null: false
-      t.integer :house
-      t.integer :life
-      t.integer :food
-      t.integer :enjoy
-      t.integer :saving
-      t.integer :investing
-      t.integer :other
+      t.integer :house, null: false
+      t.integer :life, null: false
+      t.integer :food, null: false
+      t.integer :enjoy, null: false
+      t.integer :saving, null: false
+      t.integer :investing, null: false
+      t.integer :other, null: false
       t.timestamps
     end
   end
