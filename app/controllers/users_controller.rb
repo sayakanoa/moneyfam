533ca8f9fabf4aadc4e9d0c49b7c5a602b111ba3
sayakanoa <reiluv75418@gmail.com>
2021-11-23 02:18:37 +0000

    ユーザー編集

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,4 +6,19 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to users_path
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :introduction, :money_type)
+  end
 end
