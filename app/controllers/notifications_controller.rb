bb7daa6491c65bf14bf97e8186814db00cb58d77
sayakanoa <reiluv75418@gmail.com>
2021-11-26 06:33:28 +0000

    通知機能修正

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -2,7 +2,9 @@ class NotificationsController < ApplicationController
 
  def index
     @notifications = current_user.passive_notifications.page(params[:page]).per(10)
+    #未確認の通知を探す
     @notifications.where(checked: false).each do |notification|
+    #確認済みに更新する
     notification.update_attributes(checked: true)
     end
  end
--- app/models/event.rb
+++ app/models/event.rb
@@ -5,17 +5,7 @@ class Event < ApplicationRecord
 
   validates :title, presence: true
 
-  def create_notification_comment!(current_user, post_comment_id)
-    # 自分以外にコメントしている人をすべて取得し、全員に通知を送る
-    temp_ids = PostComment.select(:user_id).where(event_id: id).where.not(user_id: current_user.id).distinct
-    temp_ids.each do |temp_id|
-      save_notification_comment!(current_user, post_comment_id, temp_id['user_id'])
-    end
-    # まだ誰もコメントしていない場合は、投稿者に通知を送る
-    save_notification_comment!(current_user, post_comment_id, user_id) if temp_ids.blank?
-  end
-
-
+  #コメントされたときに通知を作る
   def save_notification_comment!(current_user, post_comment_id, visited_id)
 
     notification = current_user.active_notifications.new(
@@ -25,6 +15,7 @@ class Event < ApplicationRecord
       action: 'comment'
     )
 
+    #通知を送る相手と通知を受け取る相手が同じであれば（つまり自分がコメントした時に自分に通知を送らないようにする設定）
     if notification.visitor_id == notification.visited_id
       notification.checked = true
     end
